7ec734b7449f2b70a18d6e71bc43633dd3f70173
mike seibel <mikes@washington.edu>
2016-04-01 16:22:42 -0700

    for statics on webapp

--- slack_bot_manager/settings.py
+++ slack_bot_manager/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'tmp')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #COMPRESSOR SETTINGS
 COMPRESS_ENABLED = False
