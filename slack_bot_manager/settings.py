cdbd4c33c4e5cdb570285b3c5c2d5411751f0eff
mike seibel <mikes@washington.edu>
2016-04-04 09:23:36 -0700

    missing os module

--- slack_bot_manager/settings.py
+++ slack_bot_manager/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Set Static file path
-PROJECT_ROOT = os.path.dirname(os.path.abspath(path.dirname(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\','/')
 
 
