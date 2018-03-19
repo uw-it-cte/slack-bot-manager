1d790f345328817f43a9bce031580b5466df946f
Bradley Bell <bradleyb@uw.edu>
2018-03-19 14:04:04 -0700

    Django 1.10 updates

--- bot_manager/urls.py
+++ bot_manager/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from bot_manager.views.bots import BotView, BotListView
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'api/v1/bot/(?P<bot_id>[0-9]+)?$', BotView().run),
     url(r'api/v1/bots/?$', BotListView().run),
-)
+]
