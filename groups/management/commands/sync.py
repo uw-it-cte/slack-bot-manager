1651de4b6312d14e39fc7cbf35be831134dbf4a7
Bradley Bell <bradleyb@uw.edu>
2018-08-21 15:57:29 -0700

    support gws v3, python 3, and add some hard-coded groups

--- groups/management/commands/sync.py
+++ groups/management/commands/sync.py
@@ -3,6 +3,7 @@ import os
 from django.core.management.base import BaseCommand, CommandError
 from uw_gws import GWS
 from slacker import Slacker
+from six.moves import html_parser
 
 
 class Command(BaseCommand):
@@ -23,41 +24,63 @@ class Command(BaseCommand):
         for user in response.body['members']:
             if user['deleted'] != False:
                 continue
-            self.stdout.write(user['name'])
-            slack_users[user['name']] = user
+            if 'email' not in user['profile']:
+                continue
+            self.stdout.write(
+                "%s\t%s\t%s" % (user['name'], user['profile']['display_name'],
+                                user['profile']['email']))
+            slack_users[user['profile']['email']] = user
 
         gws = GWS()
 
-        try:
-            gws_groups = gws.search_groups(
-                stem='u_classrm_services_serviceteam')
-        except Exception as ex:
-            raise CommandError('Could not get service team groups: %s' % ex)
+        gws_groups = {}
 
-        for gws_group in gws_groups:
-            self.stdout.write(gws_group.name[0])
+        response = gws.search_groups(stem='u_classrm_services_serviceteam')
+        for gws_group in response:
+            if isinstance(gws_group.name, tuple):
+                gws_group.name = gws_group.name[0]
+            self.stdout.write(gws_group.name)
+            handle = gws_group.name.replace(
+                'u_classrm_services_serviceteam_', '')
+            gws_groups[handle] = gws.get_group_by_id(gws_group.name)
 
-            handle = gws_group.name[0].replace(
-                'u_classrm_services_serviceteam_',
-                '')
+        gws_groups['help-desk'] = gws.get_group_by_id(
+            'u_classrm_services_function_classroom')
+
+        gws_groups['cte-all'] = gws.get_group_by_id('u_classrm_employees_all')
+
+        gws_groups['lt-all'] = gws.get_group_by_id('uw_lt')
+
+        gws_groups['ctl-all'] = gws.get_group_by_id(
+            'u_classrm_services_slack_ctl')
+
+        h = html_parser.HTMLParser()
+
+        for handle in gws_groups:
+            gws_group = gws_groups[handle]
             if handle not in slack_groups:
                 response = slack.usergroups.create(
-                    name=gws_group.title,
+                    name=h.unescape(gws_group.display_name),
                     handle=handle,
-                    description=gws_group.description)
+                    description=h.unescape(gws_group.description))
                 groupid = response.body['usergroup']['id']
             else:
                 groupid = slack_groups[handle]['id']
+                slack.usergroups.update(
+                    groupid,
+                    name=h.unescape(gws_group.display_name),
+                    description=h.unescape(gws_group.description))
 
-            gws_members = gws.get_effective_members(gws_group.name[0])
+            gws_members = gws.get_effective_members(gws_group.name)
             memberids = []
             for member in gws_members:
-                if member.member_type != 'uwnetid':
+                if member.type != 'uwnetid':
                     continue
                 self.stdout.write("\t%s" % member.name)
 
-                if member.name in slack_users:
-                    memberids.append(slack_users[member.name]['id'])
+                email = "%s@uw.edu" % member.name
+                if email in slack_users:
+                    memberids.append(slack_users[email]['id'])
 
             response = slack.usergroups.users.update(usergroup=groupid,
                                                      users=memberids)
